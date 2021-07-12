7f4424cb215c660fba40b34b5a8ae6569e2b7129
Paulo Matos <pmatos@linki.tools>
2021-07-12 19:22:26 +0200

    update gcc9 build configuration

--- build-gcc-9.sh
+++ build-gcc-9.sh
@@ -11,7 +11,7 @@ echo "ARCH: ${ARCH}"
 
 
 # Install dependencies
-apt-get install -y build-essential libc6-dev wget libmpc-dev libgmp-dev libmpfr-dev 
+apt-get install -y texinfo build-essential libc6-dev wget libmpc-dev libgmp-dev libmpfr-dev 
 
 if [ "${ARCH}" != "arm32v7" ]; then
     apt-get install -y gcc-multilib g++-multilib
@@ -36,18 +36,42 @@ if [ "${ARCH}" == "arm32v7" ]; then
                            --with-mode=thumb \
                            --disable-werror \
                            --enable-checking=yes \
-                           --program-suffix=-9 \
-                           --disable-bootstrap \
-                           --disable-multilib \
-                           --disable-docs \
-                           --disable-nls
+                           --enable-shared \
+                           --enable-linker-build-id \
+                           --libexecdir=/usr/lib \
+                           --without-included-gettext \
+                           --enable-threads=posix \
+                           --libdir=/usr/lib \
+                           --enable-nls \
+                           --enable-bootstrap \
+                           --enable-clocale=gnu \
+                           --enable-libstdcxx-debug \
+                           --enable-libstdcxx-time=yes \
+                           --with-default-libstdcxx-abi=new \
+                           --enable-gnu-unique-object \
+                           --disable-libitm \
+                           --disable-libquadmath \
+                           --disable-libquadmath-support \
+                           --enable-plugin \
+                           --enable-default-pie \
+                           --enable-objc-gc=auto \
+                           --enable-multiarch \
+                           --disable-sjlj-exceptions \
+                           --with-arch=armv7-a \
+                           --with-fpu=vfpv3-d16 \
+                           --with-float=hard \
+                           --with-mode=thumb \
+                           --disable-werror \
+                           --build=arm-linux-gnueabihf \
+                           --host=arm-linux-gnueabihf \
+                           --target=arm-linux-gnueabihf
 else
     ../gcc-9.4.0/configure --prefix=/usr \
                            --enable-languages=c,c++,lto \
                            --disable-werror \
                            --enable-checking=yes \
                            --program-suffix=-9 \
-                           --disable-bootstrap \
+                           --enable-bootstrap \
                            --disable-multilib \
                            --disable-docs \
                            --disable-nls    
